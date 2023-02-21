036c7240ea3591946bdf54025270e8b4a021f4a7
ravathu-jahnavi <jravathu@equinix.com>
2023-02-21 23:58:11 +0530

    added code amd comments

---
+++ src/net/javaguides/jsonsimple/Main.java
@@ -0,0 +1,24 @@
+package net.javaguides.jsonsimple;
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // input of file name
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter json file name: ");
+        String filename = sc.nextLine();
+
+        //object creation - empty json file is created
+        MyMongo m = new MyMongo(filename);
+
+        //insert function
+        System.out.println("insertion: "+m.insert(1,"ravathu","jahnavi", "rj@email.com"));
+        System.out.println("insertion: "+m.insert(2,"ravathu","jahnavi", "rj@email.com"));
+
+        // read function - display all
+        System.out.println(m.find());
+    }
+}
---
+++ src/net/javaguides/jsonsimple/MyMongo.java
@@ -0,0 +1,103 @@
+package net.javaguides.jsonsimple;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.io.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class MyMongo {
+    File file;
+    JSONArray userList = new JSONArray();
+
+    /**
+     * MyMongo method (constructor) here creates a new json file with the name given in parameter.
+     * @param filename
+     */
+    MyMongo(String filename)
+    {
+        try {
+            this.file = new File("./"+filename);
+            if(file.createNewFile()) {
+                System.out.println("File successfully created!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * insert function is used here to add single collection to a database
+     * @param id
+     * @param firstname
+     * @param lastname
+     * @param email
+     * @return true or false
+     */
+    public boolean insert(int id, String firstname, String lastname, String email)
+    {
+        JSONObject userDetails = new JSONObject();
+        userDetails.put("id", id);
+        userDetails.put("firstName", firstname);
+        userDetails.put("lastName", lastname);
+        userDetails.put("email", email);
+        this.userList.add(userDetails);
+
+        try(FileWriter wfile = new FileWriter(this.file)) {
+                wfile.write(String.valueOf(this.userList));
+                wfile.flush();
+                return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    /**
+     * find function with no parameters is used to display all collections inside the json file
+     * does not take any parameters
+     * @return JSONArray
+     */
+    public JSONArray find()
+    {
+        JSONParser parser = new JSONParser();
+        JSONArray arr = new JSONArray();
+        Object obj = null;
+        try {
+            //System.out.println(this.userList);
+            obj = parser.parse(new FileReader(this.file));
+            arr = (JSONArray) obj;
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            obj = new Object();
+            System.out.println("empty");
+        }
+        return arr;
+    }
+
+    /**
+     * remove method is used to delete the json file
+     * @return true or false
+     */
+    public boolean remove()
+    {
+        try (FileWriter file = new FileWriter(this.file))
+        {
+            file.write("");
+            file.flush();
+            return true;
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
