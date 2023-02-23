e4d685534ca155d2827942ab18b08c6efa70f749
ravathu-jahnavi <124995155+ravathu-jahnavi@users.noreply.github.com>
2023-02-23 18:24:33 +0530

    added drop execution and updated comments

--- src/net/javaguides/jsonsimple/Main.java
+++ src/net/javaguides/jsonsimple/Main.java
@@ -12,16 +12,19 @@ public class Main
         String filename = sc.nextLine();
 
         //object creation - empty json file is created
-        MyMongo m = new MyMongo(filename);
+        MyMongo collection = new MyMongo(filename);
 
-        //insert function
-        System.out.println("insertion: "+m.insert(1,"ravathu","jahnavi", "rj@email.com"));
-        System.out.println("insertion: "+m.insert(2,"ravathu","jahnavi", "rj@email.com"));
+        //insert function - insertion of document into collection
+        System.out.println("insertion: "+collection.insert(1,"ravathu","jahnavi", "rj@email.com"));
+        System.out.println("insertion: "+collection.insert(2,"ravathu","jahnavi", "rj@email.com"));
 
-        // read function - display all
-        System.out.println(m.find());
+        // read function - display all documents
+        System.out.println(collection.find());
         
-        // delete data - documents from collection
-        System.out.println(m.remove());
+        // delete documents from collection
+        System.out.println(collection.remove());
+        
+        // delete collection
+        System.out.println(collection.drop());
     }
 }
